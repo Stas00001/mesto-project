ade2aa076849e37dd411bed18b8e7b30cb37ae55
Stanislav Makeev <c0w8oy@yandex.ru>
2022-12-20 13:31:58 +0300

    cards

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const profile = document.querySelector('.profile');
 const profileBtn = document.querySelector('.profile__btn');
-const openPopupBtn = document.querySelectorAll('.popup-open');
+const publications = document.querySelector('.publications');
+const openPopupProfile = document.querySelectorAll('.profile__btn_type_edit');
+const openpopupAddCard = document.querySelectorAll('.profile__btn_type_add');
 const popupBtnClose = document.querySelectorAll('.popup__close-btn');
 const btnSave = document.querySelector('.popup__save-btn');
-const popup = document.querySelector('.popup');
+const popupProfile = document.getElementById('popup_profile');
+const popupAddCard = document.getElementById('popup_add');
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -30,78 +34,89 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-openPopupBtn.forEach((button) => {
+
+
+openPopupProfile.forEach((button) => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
-        popup.classList.add('popup_opened');
+        popupProfile.classList.add('popup_opened');
+        formElement.classList.add('active');
     })
 });
+
+
 
 popupBtnClose.forEach((button) => {
   button.addEventListener('click', (e) =>{
     e.preventDefault();
-    popup.classList.remove('popup_opened');
+    popupProfile.classList.remove('popup_opened');
+    popupAddCard.classList.remove('popup_opened');
+    formElement.classList.remove('active');
+
+
   })
 });
 
+openpopupAddCard.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupAddCard.classList.add('popup_opened');
+    })
+});
 
 
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
+const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input[name="name"]');
-console.log(nameInput.value);// Воспользуйтесь инструментом .querySelector()
-const jobInput = document.querySelector('.popup__input[name="job"]');// Воспользуйтесь инструментом .querySelector()
+const jobInput = document.querySelector('.popup__input[name="job"]');
 const nameProfile = document.querySelector('.profile__name');
 const aboutProfile = document.querySelector('.profile__about');
 nameInput.value = nameProfile.textContent;
 jobInput.value = aboutProfile.textContent;
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-    // Получите значение полей jobInput и nameInput из свойства value
+    evt.preventDefault();
 
- const nameInputValue  = nameInput.value;
- const jobInputValue = jobInput.value;
+    const nameInputValue  = nameInput.value;
+    const jobInputValue = jobInput.value;
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
     nameProfile.textContent = nameInputValue;
     aboutProfile.textContent = jobInputValue;
-    popup.classList.remove('popup_opened');
+    popupProfile.classList.remove('popup_opened');
 
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 
+const cardsForm = document.querySelector('.popup__form[name="cards"]');
 
-
-
-
-
-/*if(popupLinks.length > 0) {
-  for (let index=0; index<popupLinks.length; index++) {
-    const popupLink = popupLinks[index];
-    popupLink.addEventListener('click' function (e) {
-      const popupName = popopLink.getAttribute('href').replace('#',' ');
-      const currentPopup.document.getAttributeId(popupName);
-      popupOpen(currentPopup);
-      e.preventDefault();
-    });
-  }
+function addCards (namaValue, imageValue) {
+  const cardsTemplate = document.querySelector('#cards').content;
+  const card = cardsTemplate.querySelector('.publications__card').cloneNode(true);
+  card.querySelector('.publications__title').textContent = namaValue;
+  card.querySelector('.publications__image').src = imageValue;
+  card.querySelector('.publications__btnlike').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('publications__btnlike_active');
+  });
+  publications.prepend(card);
 }
 
-function popupOpen(currentPopup) {
-  if (currentPopup && unlock) {
-    const popupActive
-  }
-  currentPopup.classListAdd('.popup_opened');
+function handleFormSubmitCards(evt) {
+    evt.preventDefault();
+    const nameCard  = document.querySelector('.popup__input[name="nameCard"]');
+    const imageCard  = document.querySelector('.popup__input[name="images"]');
+    addCards(nameCard.value, imageCard.value);
+    popupAddCard.classList.remove('popup_opened');
 
 }
-*/
+cardsForm.addEventListener('submit', handleFormSubmitCards);
+
+initialCards.forEach((item) => {
+  const cardsTemplate = document.querySelector('#cards').content;
+  const card = cardsTemplate.querySelector('.publications__card').cloneNode(true);
+  card.querySelector('.publications__title').textContent = item.name;
+  card.querySelector('.publications__image').src = item.link;
+  card.querySelector('.publications__btnlike').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('publications__btnlike_active');
+  });
+  publications.append(card);
+
+});
