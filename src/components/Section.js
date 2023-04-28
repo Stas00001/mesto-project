4e6d794c3a0f596e3a36fd5f88d91b79ea7c95de
Stas00001 <116278430+Stas00001@users.noreply.github.com>
2023-04-28 20:32:51 +0300

    fix: чистка

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,21 +8,10 @@ export default class Section {
     this._container.prepend(this._renderer(element));
   }
 
-  setItemDefault(element) {
-    this._container.append(this._renderer(element));
-
-  }
-
   renderItems(cards) {
     cards.reduceRight((_, item) => {
       this.setItem(item)
     }, null)
-  }
-  
-  renderItemsDefault(cards) {
-    cards.forEach(item => {
-      this.setItemDefault(item);
-    });
   }
 }
 
